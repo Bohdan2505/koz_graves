8158ee0641d9814467e7f28df8a3514448167d7e
Bohdan <bogdan.zhuravel.00@gmail.com>
2024-08-29 15:25:44 +0300

    Add files via upload

---
+++ section_label_points.js
@@ -0,0 +1,15 @@
+let section_label_points = {
+"type": "FeatureCollection",
+"name": "section_label_points",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "section": "8" }, "geometry": { "type": "Point", "coordinates": [ 33.846451691730685, 49.201329301622472 ] } },
+{ "type": "Feature", "properties": { "section": "7" }, "geometry": { "type": "Point", "coordinates": [ 33.847057991626869, 49.201368358663089 ] } },
+{ "type": "Feature", "properties": { "section": "6" }, "geometry": { "type": "Point", "coordinates": [ 33.847680691780901, 49.201400839225819 ] } },
+{ "type": "Feature", "properties": { "section": "5" }, "geometry": { "type": "Point", "coordinates": [ 33.848247176864625, 49.201434780460694 ] } },
+{ "type": "Feature", "properties": { "section": "1" }, "geometry": { "type": "Point", "coordinates": [ 33.848154808137629, 49.202281516487616 ] } },
+{ "type": "Feature", "properties": { "section": "2" }, "geometry": { "type": "Point", "coordinates": [ 33.847580736823311, 49.202260780409837 ] } },
+{ "type": "Feature", "properties": { "section": "3" }, "geometry": { "type": "Point", "coordinates": [ 33.846944078748443, 49.202227846713157 ] } },
+{ "type": "Feature", "properties": { "section": "4" }, "geometry": { "type": "Point", "coordinates": [ 33.846343578581937, 49.202198762224199 ] } }
+]
+}
